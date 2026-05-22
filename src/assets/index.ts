f584df9d445abf7c7ab7c81252ff2c7c67f7cfa7
Bruno Riwerson Silva <riwerson@gmail.com>
2026-05-22 13:25:18 -0300

    feat: centraliza export dos assets

---
+++ src/assets/index.ts
@@ -0,0 +1,11 @@
+import anunciacao from "./images/misterios/gozosos/anunciacao.png";
+import visitacao from "./images/misterios/gozosos/visitacao.png";
+import nascimento from "./images/misterios/gozosos/nascimento.png";
+
+export const ilustracoes = {
+  misteriosGozosos: {
+    anunciacao,
+    visitacao,
+    nascimento,
+  },
+};
