1d3107f9487858981ae1b7bf7100a227547ac87b
Bruno Riwerson Silva <riwerson@gmail.com>
2026-05-22 13:32:32 -0300

    feat: centralização do export dos textos estáticos

---
+++ src/data/index.ts
@@ -0,0 +1,16 @@
+export { agradecimento } from "./agradecimento";
+export { aoEspiritoSanto } from "./aoEspiritoSanto";
+export { aveMaria } from "./aveMaria";
+export { credo } from "./credo";
+export { jaculatoria } from "./jaculatoria";
+export { oferecimento } from "./oferecimento";
+export { paiNosso } from "./paiNosso";
+export { salveRainha } from "./salveRainha";
+
+export { DIAS_DA_SEMANA, type DiaDaSemana } from "./constants/DiasDaSemana";
+export { TIPOS_TERCO, type TipoTerco } from "./constants/TiposTerco";
+
+export { misteriosDolorosos } from "./tercos/misteriosDolorosos";
+export { misteriosGloriosos } from "./tercos/misteriosGloriosos";
+export { misteriosGozosos } from "./tercos/misteriosGozosos";
+export { misteriosLuminosos } from "./tercos/misteriosLuminosos";
