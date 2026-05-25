a372f3cf3d6d4ac96a4d04aa04bc1f649e9d9743
Bruno Riwerson Silva <riwerson@gmail.com>
2026-05-25 10:59:24 -0300

    chore: atualiza arquivo de exportação

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,10 +2,12 @@ export { agradecimento } from "./agradecimento";
 export { aoEspiritoSanto } from "./aoEspiritoSanto";
 export { aveMaria } from "./aveMaria";
 export { credo } from "./credo";
+export { gloria } from "./gloria";
 export { jaculatoria } from "./jaculatoria";
 export { oferecimento } from "./oferecimento";
 export { paiNosso } from "./paiNosso";
 export { salveRainha } from "./salveRainha";
+export { sinalDaCruz } from "./sinalDaCruz";
 
 export { DIAS_DA_SEMANA, type DiaDaSemana } from "./constants/DiasDaSemana";
 export { TIPOS_TERCO, type TipoTerco } from "./constants/TiposTerco";
