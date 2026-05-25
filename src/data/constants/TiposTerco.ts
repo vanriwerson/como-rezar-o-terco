d400a9342004d9acf943e5c22730965c2a31ab27
Bruno Riwerson Silva <riwerson@gmail.com>
2026-05-25 11:12:50 -0300

    refactor: melhora nome da constante dos titulos dos mistérios contemplados em cada terço do rosário

--- src/data/constants/TiposTerco.ts
+++
@@ -1,8 +0,0 @@
-export const TIPOS_TERCO = {
-  GOZOSOS: "Mistérios Gozosos",
-  DOLOROSOS: "Mistérios Dolorosos",
-  GLORIOSOS: "Mistérios Gloriosos",
-  LUMINOSOS: "Mistérios Luminosos",
-} as const;
-
-export type TipoTerco = (typeof TIPOS_TERCO)[keyof typeof TIPOS_TERCO];
---
+++ src/data/constants/TitulosTerco.ts
@@ -0,0 +1,10 @@
+export const TITULOS_TERCO = {
+  GOZOSOS: "Contemplação dos Mistérios Gozosos sobre a infância de Jesus",
+  DOLOROSOS: "Contemplação dos Mistérios Dolorosos da paixão e morte de Jesus",
+  GLORIOSOS:
+    "Contemplação dos Mistérios Gloriosos sobre a ressurreição de Jesus e assunção de Sua mãe Maria",
+  LUMINOSOS:
+    "Contemplação dos Mistérios Luminosos da Luz e dos ensinamentos de Jesus",
+} as const;
+
+export type TipoTerco = (typeof TITULOS_TERCO)[keyof typeof TITULOS_TERCO];
