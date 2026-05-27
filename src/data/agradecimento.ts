465257b7aec2c4e99fe37ad84c50d5c32df5e399
Bruno Riwerson Silva <riwerson@gmail.com>
2026-05-27 10:51:42 -0300

    refactor: altera divisão das frases nos arrays para melhor legibilidade no accordion

--- src/data/agradecimento.ts
+++ src/data/agradecimento.ts
@@ -4,7 +4,6 @@ export const agradecimento: Oracao = {
   titulo: "Agradecimento do Terço",
   texto: [
     "Infinitas graças vos damos, Soberana Rainha, pelos benefícios que todos os dias recebemos de vossas mãos liberais.",
-    "Dignai-vos agora e para sempre tomar-nos debaixo de vosso poderoso amparo e, para mais vos agradar,",
-    "vos saudamos com uma Salve Rainha.",
+    "Dignai-vos agora e para sempre tomar-nos debaixo de vosso poderoso amparo e, para mais vos agradar, vos saudamos com uma Salve Rainha.",
   ],
 };
--- src/data/salveRainha.ts
+++ src/data/salveRainha.ts
@@ -5,9 +5,7 @@ export const salveRainha: Oracao = {
   texto: [
     "Salve Rainha, Mãe de Misericórdia, vida, doçura e esperança nossa, salve!",
     "A vós bradamos os degredados filhos de Eva. A vós suspiramos, gemendo e chorando neste vale de lágrimas.",
-    "Eia, pois, advogada nossa, esses vossos olhos misericordiosos a nós volvei,",
-    "e depois deste desterro mostrai-nos Jesus, bendito fruto do vosso ventre,",
-    "ó clemente, ó piedosa, ó doce e sempre Virgem Maria.",
+    "Eia, pois, advogada nossa, esses vossos olhos misericordiosos a nós volvei, e depois deste desterro mostrai-nos Jesus, bendito fruto do vosso ventre, ó clemente, ó piedosa, ó doce e sempre Virgem Maria.",
     "Rogai por nós, Santa Mãe de Deus, para que sejamos dignos das promessas de Cristo.",
     "Amém",
   ],
