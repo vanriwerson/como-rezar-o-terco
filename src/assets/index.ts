57ed8b3d976ee6591b1f819f5803fd4d90a56880
Bruno Riwerson Silva <riwerson@gmail.com>
2026-07-08 11:45:58 -0300

    fix: ajusta caminho do arquivo

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,7 +2,7 @@ import anunciacao from "./images/misterios/gozosos/anunciacao.png";
 import visitacao from "./images/misterios/gozosos/visitacao.png";
 import nascimento from "./images/misterios/gozosos/nascimento.png";
 import apresentacao from "./images/misterios/gozosos/apresentacao.png";
-import perdaEncontro from "./images/misterios/dolorosos/perda-e-encontro.png";
+import perdaEncontro from "./images/misterios/gozosos/perda-e-encontro.png";
 
 import agonia from "./images/misterios/dolorosos/agonia.png";
 import flagelacao from "./images/misterios/dolorosos/flagelacao.png";
